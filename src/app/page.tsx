df832656f9ddbdedae37a3ab8efd5b8b11370bae
Jameson Williams <jameson@nosemaj.org>
2024-01-31 07:03:37 -0600

    Update splash page with CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <Image 
-  src="/welcome.jpg"
-  alt="DIY Discord" 
-  width="500"
-  height="500"
-  priority={true}
-  />;
+  return (
+    <>
+      <Link target="_blank" href="https://discord.gg/diyordie">
+        <p>Join the discord server!</p>
+        <Image
+          src="/welcome.jpg"
+          alt="DIY Discord"
+          width="500"
+          height="500"
+          priority={true}
+          className="rounded-2xl"
+        />
+      </Link>
+    </>
+  );
 }
