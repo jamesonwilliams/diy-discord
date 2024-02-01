155e024b4a140b9dbe46852463c73aad67e24f5a
Jameson Williams <jameson@nosemaj.org>
2024-02-01 17:05:05 -0600

    Fix bandcamp icon svg attributes

--- src/app/components/social-icons/BandcampIcon.tsx
+++ src/app/components/social-icons/BandcampIcon.tsx
@@ -9,13 +9,13 @@ export default function BandcampIcon() {
         viewBox="0 0 30 30"
         width="24px"
         height="24px"
-        clip-rule="evenodd"
+        clipRule="evenodd"
         className={styles.icon}
       >
         <path
           d="M15,3C8.373,3,3,8.373,3,15s5.373,12,12,12s12-5.373,12-12S21.627,3,15,3z M17.333,18.774H8.578l4.089-7.547h8.755L17.333,18.774z"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
     </div>
